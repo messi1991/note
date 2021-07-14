f3d4a8c11c952b88f6959974af4185b6f2ceb281
gaocunli <603375508@qq.com>
2021-07-14 23:16:00 +0800

    no message

---
+++ java/MiGong/MiGong.java
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-14 23:02:31
+ * @LastEditTime: 2021-07-14 23:07:56
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /notes/java/MiGong/MiGong.java
+ */
+public class MiGong {
+  public static void main(String[] args) {
+    int [][] map = new int[8][7];
+    System.out.println("数组长度" + map.length);
+  }
+}
