68c466c0ff36cac41b00ae0e935ff03d5080c9f6
gaocunli <gaocunli@erwan365.com>
2023-08-10 19:17:38 +0800

    up

--- javaScript/es/demo.js
+++ javaScript/es/demo.js
@@ -1,8 +1,8 @@
-#!/usr/bin/env node
+/* #!/usr/bin/env node
 
 'use strict';
 console.log('1');
-console.log('windows', globalThis === global)
+console.log('windows', globalThis === global) */
 
 /* const a = []
 for(let i = 1; i <= 10; i++) {
@@ -13,26 +13,71 @@ console.log('findLastIndex', a.findLastIndex(el => el < 6));
 
 console.log('a', a)
 console.log('with', a.with(0, 2))
-console.log('a', a)
+console.log('a', a) */
 
 
-const delay = (value, ms) => new Promise((resolve, reject) => setTimeout(() => resolve(value), ms));
+/* const delay = (value, ms, isError) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('setTimeout', value)
+    if(isError) {
+      reject(new Error(`reject ${value}`))
+    }
+    resolve(value)
+  }, ms);
+});
+
 const promises = [
-  delay('a', 3000),
-  delay('b', 2000),
+  delay('a', 2000),
+  delay('b', 3000, true),
   delay('c', 4000),
-];
+]; */
 
-Promise.any(promises)
-  .then(res => console.log(res)) // b
-  .catch(err => console.error(err.name, err.message, err.errors)) // 全部失败时返回：AggregateError All promises were rejected [ 'a', 'b', 'c' ]
+/* Promise.any(promises)
+  .then(res => console.log('Promise.any', res))
+  .catch(err => console.error(err.name, err.message, err.errors)) */
+
+/* Promise.race(promises)
+  .then(res => console.log('Promise.race', res))
+  .catch(err => console.error(err.name, err.message, err.errors)) */
 
 
+/* Promise.allSettled(promises)
+  .then(res => console.log('Promise.allSettled', res))
+  .catch(err => console.error(err.name, err.message, err.errors)) */
+
+/* Promise.all(promises)
+  .then(res => console.log('Promise.all', res))
+  .catch(err => console.error(err.name, err.message, err.errors)) */
+
+
+/* class Person {
+  #name="gao"
+  getName() {
+    return this.#name
+  }
+}
+
+const a = new Person()
+
+console.log(a.getName())
+console.log(a.#name) */
+
+
+
+/* var obj = Object.create(null)
+
+console.log(obj.hasOwnProperty('a'))
+console.log(Object.hasOwn(obj, 'a')) */
+
+/* const obj = { name: 'Tom', age: 18 }
+
+console.log('Object.values', Object.values(obj))
+console.log('Object.keys', Object.keys(obj))
+
+const objArr = Object.entries(obj)
+console.log('Object.entries', objArr)
+for (const [key, value] of objArr) {
+  console.log(key, value);
+}
  */
-
-//  #!/usr/bin/env node
-
-// #!D:/Program Files/nodejs node
-
-
 
